d5cbecb0d903cdb2090f28a381a4f4ab04e889b0
tinysunshine <hsc802@gmail.com>
2014-07-21 09:53:09 +0800

    冲突

--- CallCenter/src/main/java/com/redcard/customer/dao/CustomerDao.java
+++ CallCenter/src/main/java/com/redcard/customer/dao/CustomerDao.java
@@ -5,10 +5,18 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.redcard.customer.entity.Customer;
 
+import java.util.List;
+
 public interface CustomerDao extends PagingAndSortingRepository <Customer,String> {
 	@Query("select count(m) from Customer m where m.fldName = ?1 and m.fldPhone = ?2")
     public Long countByPhone(String name,String phone);
 	
 	@Query("select count(m) from Customer m where m.fldName = ?1 and m.fldMobile = ?2")
     public Long countByMobile(String name,String mobile);
+
+    @Query("select c from Customer c where c.fldMobile = ?1 and c.fldStatus = 0 ")
+    public List<Customer> findCustomerByMobile(String mobile);
+
+    @Query("select c from Customer c where c.fldPhone = ?1 and c.fldStatus = 0")
+    public List<Customer> findCustomerByPhone(String phone);
 }
