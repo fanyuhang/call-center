2c1150e5adb85c07aa2ab84589cf2673e8a4a7bf
wsjerome <wsj19880518@126.com>
2014-08-10 21:05:01 +0800

    新增邮件管理相关菜单

--- CallCenter/src/main/java/com/redcard/message/dao/MessageOperateDao.java
+++ CallCenter/src/main/java/com/redcard/message/dao/MessageOperateDao.java
@@ -1,11 +1,8 @@
 package com.redcard.message.dao;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.redcard.message.entity.MessageOperate;
-
-import java.util.List;
 
 public interface MessageOperateDao extends PagingAndSortingRepository<MessageOperate, String> {
 
